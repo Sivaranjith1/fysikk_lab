274ae39ed15374a4210512dbce15b41530d011c8
Sivaranjith Sivarasa <sivaranjith.sivarasa@juniorconsulting.no>
2020-09-30 05:47:15 +0200

    Showing graph from choosen file

--- cubicspline.py
+++ cubicspline.py
@@ -69,28 +69,27 @@ xfast=np.asarray([0,h,2*h,3*h,4*h,5*h,6*h,7*h])
 xfast = xfast/1000
 yfast = np.asarray([0.255,0.183,0.189,0.188,0.129,0.091,0.151,0.171])
 
-dataFromFile = np.loadtxt('Datafiles/1934.txt', skiprows=2)
+dataFromFile = np.loadtxt('Datafiles/1930.txt', skiprows=2)
 
 xfast = []
 yfast = []
+xmin = dataFromFile[0][1]
 for data in dataFromFile:
-    xfast.append(data[1])
+    xfast.append(data[1]- xmin)
     yfast.append(data[2])
 
-xfast = np.asarray(xfast)
-yfast = np.asarray(yfast)
+xfast = np.array(xfast)
+yfast = np.array(yfast)
 
-print(xfast, yfast)
-
-xfast= xfast/1000
+print('xfast' , xfast, '\n yfast',yfast, '\n', len(xfast), '\n yfast: ', len(yfast))
 
 #Programmet beregner deretter de 7 tredjegradspolynomene, et
 #for hvert intervall mellom to nabofestepunkter.
 
 #Med scipy.interpolate-funksjonen CubicSpline:
 cs = CubicSpline(xfast, yfast, bc_type='natural')
-xmin = 0.000
-xmax = 1.401
+xmin = xfast[0]
+xmax = xfast[-1]
 dx = 0.001
 x = np.arange(xmin, xmax, dx)
 Nx = len(x)
@@ -98,25 +97,25 @@ y = cs(x)
 dy = cs(x,1)
 d2y = cs(x,2)
 g = 9.81
-y0 = 0.255
+y0 = yfast[0]
 c_ball = 2/5
 mass = 1
 beta_rad = np.arctan(dy)
 beta_deg = 180*beta_rad/np.pi
-# v = np.sqrt((2*g*(y0 - y)/(1+c_ball)))
-# curl = d2y/pow((1+pow(dy,2)),3/2)
-# a_perp = v*v*curl
-# acc = -g*np.sin(beta_rad)/(1+c_ball)
-# f = (c_ball*mass*g*np.sin(beta_rad))/(1+c_ball)
+v = np.sqrt((2*g*(y0 - y)/(1+c_ball)))
+curl = d2y/pow((1+pow(dy,2)),3/2)
+a_perp = v*v*curl
+acc = -g*np.sin(beta_rad)/(1+c_ball)
+f = (c_ball*mass*g*np.sin(beta_rad))/(1+c_ball)
 
-# N = mass*(g*np.cos(beta_rad) + a_perp)
+N = mass*(g*np.cos(beta_rad) + a_perp)
 
-v= 0
-curl = 0
-a_prep = 0
-acc = 0
-f = 0
-N = 0
+# v= 0
+# curl = 0
+# a_prep = 0
+# acc = 0
+# f = 0
+# N = 0
 #Plotting
 
 baneform = plt.figure('y(x)',figsize=(12,3))
@@ -124,120 +123,122 @@ plt.plot(x,y,xfast,yfast,'*')
 plt.title('Form')
 plt.xlabel('x ($m$)',fontsize=20)
 plt.ylabel('y(x) ($m$)',fontsize=20)
-plt.ylim(0,0.350)
+plt.ylim(0,yfast[-1]*1.5)
 plt.grid()
-#baneform.savefig("baneform.pdf", bbox_inches='tight')
-#baneform.savefig("baneform.png", bbox_inches='tight')
+baneform.savefig("baneform.pdf", bbox_inches='tight')
+baneform.savefig("baneform.png", bbox_inches='tight')
 
-baneform = plt.figure('\u03BA',figsize=(12,3))
-plt.plot(x,curl)
-plt.title('\u03BA')
-plt.xlabel('x ($m$)',fontsize=20)
-plt.ylabel('\u03BA(x) ($1/m$)',fontsize=20)
-# plt.ylim(0,0.350)
-plt.grid()
-# plt.show()
+print('y', y, len(y))
 
-# print('Antall forsøk',attempts)
-print('Festepunkthøyder (m)',yfast)
-print('Banens høyeste punkt (m)',np.max(y))
+# baneform = plt.figure('\u03BA',figsize=(12,3))
+# plt.plot(x,curl)
+# plt.title('\u03BA')
+# plt.xlabel('x ($m$)',fontsize=20)
+# plt.ylabel('\u03BA(x) ($1/m$)',fontsize=20)
+# # plt.ylim(0,0.350)
+# plt.grid()
+# # plt.show()
 
-print('NB: SKRIV NED festepunkthøydene når du/dere er fornøyd med banen.')
-print('Eller kjør programmet på nytt inntil en attraktiv baneform vises.')
+# # print('Antall forsøk',attempts)
+# print('Festepunkthøyder (m)',yfast)
+# print('Banens høyeste punkt (m)',np.max(y))
 
-graf_beta = plt.figure('\u03b2(x)',figsize=(12,3))
-plt.grid()
-plt.title('\u03b2')
-plt.xlabel('x ($m$)')
-plt.ylabel('\u03b2 ($\u00B0$)')
-plt.plot(x, beta_deg)
+# print('NB: SKRIV NED festepunkthøydene når du/dere er fornøyd med banen.')
+# print('Eller kjør programmet på nytt inntil en attraktiv baneform vises.')
 
-graf_v = plt.figure('Velocity',figsize=(12,3))
-plt.grid()
-plt.title('Velocity')
-plt.xlabel('x ($m$)')
-plt.ylabel('v ($m/s$)')
-plt.plot(x,v)
+# graf_beta = plt.figure('\u03b2(x)',figsize=(12,3))
+# plt.grid()
+# plt.title('\u03b2')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('\u03b2 ($\u00B0$)')
+# plt.plot(x, beta_deg)
 
-graf_a_perp = plt.figure('Centripetal Acceleration(x)',figsize=(12,3))
-plt.grid()
-plt.title('Centripetal Acceleration')
-plt.xlabel('x ($m$)')
-plt.ylabel('Centripetal Acceleration ($m/s\u00B2$)')
-plt.plot(x,a_perp)
+# graf_v = plt.figure('Velocity',figsize=(12,3))
+# plt.grid()
+# plt.title('Velocity')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('v ($m/s$)')
+# plt.plot(x,v)
 
-graf_acc = plt.figure('Acceleration(x)',figsize=(12,3))
-plt.grid()
-plt.title('Acceleration')
-plt.xlabel('x ($m$)')
-plt.ylabel('Acceleration ($m/s\u00B2$)')
-plt.plot(x,acc)
+# graf_a_perp = plt.figure('Centripetal Acceleration(x)',figsize=(12,3))
+# plt.grid()
+# plt.title('Centripetal Acceleration')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('Centripetal Acceleration ($m/s\u00B2$)')
+# plt.plot(x,a_perp)
 
-normal_force = plt.figure('N(t)', figsize=(12,3))
-plt.grid()
-plt.title('Normal Force')
-plt.xlabel('x ($m$)')
-plt.ylabel('Normal force ($kg\u00B7m/s\u00B2$)')
-plt.plot(x,N)
+# graf_acc = plt.figure('Acceleration(x)',figsize=(12,3))
+# plt.grid()
+# plt.title('Acceleration')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('Acceleration ($m/s\u00B2$)')
+# plt.plot(x,acc)
 
-v_x = v*np.cos(beta_rad)
-v_x_avg = np.zeros(len(v_x))
-v_x_avg[0] = v_x[0]
-dt = np.zeros(len(v_x))
+# normal_force = plt.figure('N(t)', figsize=(12,3))
+# plt.grid()
+# plt.title('Normal Force')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('Normal force ($kg\u00B7m/s\u00B2$)')
+# plt.plot(x,N)
 
-graf_v_x = plt.figure('v_x',figsize=(12,3))
-plt.grid()
-plt.title('Velocity in x-direction')
-plt.plot(x,v_x)
+# v_x = v*np.cos(beta_rad)
+# v_x_avg = np.zeros(len(v_x))
+# v_x_avg[0] = v_x[0]
+# dt = np.zeros(len(v_x))
 
-for i in range(1,len(v_x)):
-    v_x_avg[i] = 0.5*(v_x[i-1] + v_x[i])
-    dt[i] = dx/v_x_avg[i]
+# graf_v_x = plt.figure('v_x',figsize=(12,3))
+# plt.grid()
+# plt.title('Velocity in x-direction')
+# plt.plot(x,v_x)
+
+# for i in range(1,len(v_x)):
+#     v_x_avg[i] = 0.5*(v_x[i-1] + v_x[i])
+#     dt[i] = dx/v_x_avg[i]
     
-graf_v_x_avg = plt.figure('v_x_avg',figsize=(12,3))
-plt.grid()
-plt.title('Average velocity in x-direction')
-plt.plot(x,v_x_avg)
+# graf_v_x_avg = plt.figure('v_x_avg',figsize=(12,3))
+# plt.grid()
+# plt.title('Average velocity in x-direction')
+# plt.plot(x,v_x_avg)
 
-t = np.zeros(len(dt))
-for i in range(1,len(dt)):
-    t[i] = t[i-1]+dt[i]
+# t = np.zeros(len(dt))
+# for i in range(1,len(dt)):
+#     t[i] = t[i-1]+dt[i]
 
-graf_x_t = plt.figure('x(t)',figsize=(12,3))
-plt.grid()
-plt.title('X-position with time')
-plt.xlabel('Time (s)')
-plt.ylabel('X position (m)')
-plt.plot(t,x)
+# graf_x_t = plt.figure('x(t)',figsize=(12,3))
+# plt.grid()
+# plt.title('X-position with time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('X position (m)')
+# plt.plot(t,x)
 
-graf_y_t = plt.figure('y(t)',figsize=(12,3))
-plt.grid()
-plt.title('Y-position with time')
-plt.xlabel('Time (s)')
-plt.ylabel('Y position (m)')
-plt.plot(t,y)
+# graf_y_t = plt.figure('y(t)',figsize=(12,3))
+# plt.grid()
+# plt.title('Y-position with time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Y position (m)')
+# plt.plot(t,y)
 
-graf_v_t = plt.figure('v(t)',figsize=(12,3))
-plt.grid()
-plt.title('Velocity with time')
-plt.xlabel('Time (s)')
-plt.ylabel('Velocity (ms^-1)')
-plt.plot(t,v)
+# graf_v_t = plt.figure('v(t)',figsize=(12,3))
+# plt.grid()
+# plt.title('Velocity with time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Velocity (ms^-1)')
+# plt.plot(t,v)
 
-graf_acc_t = plt.figure('a(t)',figsize=(12,3))
-plt.grid()
-plt.title('Acceleration with time')
-plt.xlabel('Time (s)')
-plt.ylabel('Acceleration (ms^-2)')
-plt.plot(t,acc)
+# graf_acc_t = plt.figure('a(t)',figsize=(12,3))
+# plt.grid()
+# plt.title('Acceleration with time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Acceleration (ms^-2)')
+# plt.plot(t,acc)
 
-relation_f_N = np.abs(f/N)
-relation = plt.figure('|f/N|',figsize=(12,3))
-plt.title('Relation between F and N')
-plt.xlabel('x ($m$)')
-plt.ylabel('|f/N|')
-plt.grid()
-plt.plot(x,relation_f_N)
+# relation_f_N = np.abs(f/N)
+# relation = plt.figure('|f/N|',figsize=(12,3))
+# plt.title('Relation between F and N')
+# plt.xlabel('x ($m$)')
+# plt.ylabel('|f/N|')
+# plt.grid()
+# plt.plot(x,relation_f_N)
 plt.show()
 
 
